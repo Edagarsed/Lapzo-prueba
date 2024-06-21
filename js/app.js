e10fabe1e026b6a4efc5e260e9680a49bb465250
Edagarsed <alejo.rodriguez.coy@gmail.com>
2024-06-21 13:36:22 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,34 +26,36 @@ function menuBtnChange() {
 
 // TABS
 
-'use strict'
 
-const li        = document.querySelectorAll('.li')
-const bloque    = document.querySelectorAll('.bloque')
+const liItems     = document.querySelectorAll('.li')
+const bloqueItems = document.querySelectorAll('.bloque')
 
 // CLICK en li
-    // TODOS .li quitar la clase activo
-    // TODOS .bloque quitar la clase activo
-    // .li con la posicion se añadimos la clase activo
-    // .bloque con la posicion se añadimos la clase activo
+// TODOS .li quitar la clase activo
+// TODOS .bloque quitar la clase activo
+// .li con la posicion se añadimos la clase activo
+// .bloque con la posicion se añadimos la clase activo
 
 // Recorriendo todos los LI
-li.forEach( ( cadaLi , i )=>{
-    // Asignando un CLICK a CADALI
-    li[i].addEventListener('click',()=>{
+liItems.forEach((li, index) => {
+  // Asignando un CLICK a liItem
+  li.addEventListener("click", () => {
+    // Quitando la clase activo a todos los li
+    liItems.forEach((li) => li.classList.remove("activo"));
+    // Quitando la clase activo a todos los bloques
+    bloqueItems.forEach((bloque) => bloque.classList.remove("activo"));
+    // En el li que hemos click le añadimos la clase activo
+    li.classList.add("activo");
 
-        // Recorrer TODOS los .li
-        li.forEach( ( cadaLi , i )=>{
-            // Quitando la clase activo de cada li
-            li[i].classList.remove('activo')
-            // Quitando la clase activo de cada bloque
-            bloque[i].classList.remove('activo')
-        })
+    // En el bloque con la misma posición le añadimos la clase activo
+    // Son equivalentes:
+    // 1.
+    bloqueItems[index]?.classList.add("activo");
+    // 2.
+    // if (bloqueItems[index]) {
+    //   bloqueItems[index].classList.add("activo");
+    // }
+  });
 
-        // En el li que hemos click le añadimos la clase activo
-        li[i].classList.add('activo')
-        // En el bloque con la misma posición le añadimos la clase activo
-        bloque[i].classList.add('activo')
-
-    })
-})
+  
+});
